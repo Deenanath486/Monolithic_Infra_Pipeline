bdc7841f3f0da84db6d226f0cc9ef70947c27381
Deenanath Yadav <deenanathyadav106@gmail.com>
2026-08-25 01:56:00 +0530

    Add Terraform infrastructure code

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -5,12 +5,7 @@ terraform {
       version = "5.2.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "rg-state-storage"
-    storage_account_name = "axionstatestore10432"
-    container_name       = "tfstate"
-    key                  = "dev/terraform.tfstate"
-  }
+
 }
 
 provider "azurerm" {
